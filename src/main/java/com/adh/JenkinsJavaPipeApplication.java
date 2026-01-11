28d3f95eb7c1b9e1340f604c8dc178e3962629dc
Girish@77 <ginamdar70@gmail.com>
2026-01-11 22:09:43 +0530

    Project Created

---
+++ src/main/java/com/adh/JenkinsJavaPipeApplication.java
@@ -0,0 +1,13 @@
+package com.adh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsJavaPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsJavaPipeApplication.class, args);
+	}
+
+}
